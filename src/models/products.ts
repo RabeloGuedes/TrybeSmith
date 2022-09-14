c61491184c71e61b02dc6fa246d7bee93e17c488
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-13 21:08:54 -0300

    refactory: Altera a classe ProductModel.

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,20 +1,17 @@
 import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
+import { ProductBodyInterface, ProductInterface } from '../interfaces/products';
 
 export default class ProductModel {
-  name: string;
-
-  amount: string;
-
-  constructor(name: string, amount: string) {
+  constructor(public name: string, public amount: string) {
     this.name = name;
     this.amount = amount;
   }
 
-  async registerProduct() {
+  async registerProduct(product: ProductBodyInterface): Promise<ProductInterface> {
     const [{ insertId }] = await connection
       .execute<ResultSetHeader>(`INSERT INTO Trybesmith.Products
-    (name, amount) VALUES (?, ?)`, [this.name, this.amount]);
+    (name, amount) VALUES (?, ?)`, [product.name, product.amount]);
     return ({
       id: insertId,
       name: this.name,
