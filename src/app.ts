8a5bf5b050a0b298388d7b3df8e19a2f0f68e9ad
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 19:33:11 -0300

    feat: Adiciona a rota 'loginRouter' ao 'app.ts'.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import ordersRouter from './routes/orders';
 import productRouter from './routes/product';
 import userRouter from './routes/users';
+import loginRouter from './routes/login';
 
 const app = express();
 
@@ -9,5 +10,6 @@ app.use(express.json());
 app.use('/products', productRouter);
 app.use('/users', userRouter);
 app.use('/orders', ordersRouter);
+app.use('/login', loginRouter);
 
 export default app;
