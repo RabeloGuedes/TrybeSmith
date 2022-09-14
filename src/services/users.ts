f7257c45e2d278547eba5f87ade0f5774be0a0e7
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 15:12:43 -0300

    feat: Cria a classe 'UserService' com o método 'registerUser' que retorna o token.

---
+++ src/services/users.ts
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import UserInterface from '../interfaces/users';
+import UserModel from '../models/users';
+import tokenInfos from '../util';
+
+export default class UserService {
+  model: UserModel;
+
+  constructor() {
+    this.model = new UserModel();
+  }
+
+  async registerUser(user: UserInterface): Promise<string> {
+    const id = await this.model.registerUser(user);
+    const token = jwt.sign({ id }, tokenInfos.secret);
+    return token;
+  }
+}
