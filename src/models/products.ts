f840738ac342dd28b010f77ea52a07a0e00230d3
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 13:46:52 -0300

    refactory: Altera a classe ProductModel, fica mais limpa e coesa.

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,29 +1,30 @@
-import { ResultSetHeader } from 'mysql2/promise';
+import { ResultSetHeader, Pool } from 'mysql2/promise';
 import connection from './connection';
 import { ProductBodyInterface, ProductInterface } from '../interfaces/products';
 
 export default class ProductModel {
-  constructor(public name: string, public amount: string) {
-    this.name = name;
-    this.amount = amount;
+  connection: Pool;
+
+  constructor() {
+    this.connection = connection;
   }
 
   async registerProduct(product: ProductBodyInterface): Promise<ProductInterface> {
-    const [{ insertId }] = await connection
+    const [{ insertId }] = await this.connection
       .execute<ResultSetHeader>(`INSERT INTO Trybesmith.Products
     (name, amount) VALUES (?, ?)`, [product.name, product.amount]);
     return ({
       id: insertId,
-      name: this.name,
-      amount: this.amount,
+      name: product.name,
+      amount: product.amount,
     });
   }
 
-  async getAllProducts() {
-    console.log(this.name);
-    const [products] = await connection.execute(
-      'SELECT id, name, amount, orderId FROM Trybesmith.Products',
-    );
-    return products;
-  }
+  // async getAllProducts(): Promise<ProductInterface[]> {
+  //   console.log(this.name);
+  //   const [products] = await connection.execute(
+  //     'SELECT id, name, amount, orderId FROM Trybesmith.Products',
+  //   );
+  //   return products as ProductInterface[];
+  // }
 }
