8d54283f83c8544c5649a0198343246a7053d4d2
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 13:49:28 -0300

    refactory: Diminui a função chamada na rota productRouter no endpoint 'post'.

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -3,10 +3,8 @@ import ProductController from '../controllers/products';
 
 const productRouter = express.Router();
 
-productRouter.post('/', async (req, res) => {
-  const { name, amount } = req.body;
-  const productController = new ProductController(name, amount);
-  await productController.registerProduct(req, res);
-});
+const productController = new ProductController();
+
+productRouter.post('/', (req, res) => productController.registerProduct(req, res));
 
 export default productRouter;
