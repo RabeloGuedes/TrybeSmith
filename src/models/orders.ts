e2ab21ebe2d236cf030e660a7aedaf78e0d143f7
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 17:02:15 -0300

    fix: Repara erro de escritra do nome do banco de dados 'Trybesmith'.

--- src/models/orders.ts
+++ src/models/orders.ts
@@ -12,7 +12,7 @@ export default class OrdersModel {
   async getAllOrders(): Promise<OrdersInterface[]> {
     const [orders] = await this.connection.execute<RowDataPacket[]>(`
     SELECT o.id, o.userId, p.products FROM Trybesmith.Orders AS o
-    INNER JOIN Trynesmith.Products AS p
+    INNER JOIN Trybesmith.Products AS p
     ON o.id = p.orderId`);
     return orders as OrdersInterface[];
   }
