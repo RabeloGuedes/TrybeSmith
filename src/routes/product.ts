4a5da919f0603bb19aa66713c7de034017bcef67
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 13:55:10 -0300

    feat: Adiciona novo endpoint à rota 'productRouter'.

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -6,5 +6,6 @@ const productRouter = express.Router();
 const productController = new ProductController();
 
 productRouter.post('/', (req, res) => productController.registerProduct(req, res));
+productRouter.get('/', (_req, res) => productController.getAllProducts(res));
 
 export default productRouter;
