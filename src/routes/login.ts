289d8367eb412fbb3bf18a814d3585d67b687704
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 20:35:19 -0300

    feat: Adiciona middleware à rota 'loginRouter'.

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,10 +1,21 @@
 import express from 'express';
 import UserController from '../controllers/users';
+import {
+  isThereAUsername,
+  isThereAPassword,
+  areTheCredentialsValid,
+} from '../middlewares/login';
 
 const loginRouter = express.Router();
 
 const userController = new UserController();
 
-loginRouter.post('/', (req, res) => userController.login(req, res));
+loginRouter.post(
+  '/',
+  isThereAUsername,
+  isThereAPassword,
+  areTheCredentialsValid,
+  (req, res) => userController.login(req, res),
+);
 
 export default loginRouter;
