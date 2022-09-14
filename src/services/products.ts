d6bb0a20e9658e2991199217ce827df471292d8b
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-13 21:49:58 -0300

    feat: Cria a classe 'ProductService' que contém o método 'regitsterProduct' para cadastrar novos produtos.

---
+++ src/services/products.ts
@@ -0,0 +1,16 @@
+import { ProductBodyInterface } from '../interfaces/products';
+import ProductModel from '../models/products';
+
+export default class ProductService {
+  constructor(public name: string, public amount: string) {
+    this.name = name;
+    this.amount = amount;
+  }
+
+  model = new ProductModel(this.name, this.amount);
+
+  async registerProduct(product: ProductBodyInterface) {
+    const newProduct = await this.model.registerProduct(product);
+    return newProduct;
+  }
+}
