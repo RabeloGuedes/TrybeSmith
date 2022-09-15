e635347d2dd39445031e8a91aead705227a8eda5
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 21:03:00 -0300

    feat: Adiciona middleware à rota 'productRouter'.

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 import ProductController from '../controllers/products';
+import {
+  isThereName,
+  isNameTypeString,
+  isNameValid,
+  isThereAmonut,
+  isAmounTypeString,
+  isAmountValid,
+} from '../middlewares/products';
 
 const productRouter = express.Router();
 
 const productController = new ProductController();
 
-productRouter.post('/', (req, res) => productController.registerProduct(req, res));
+productRouter.post(
+  '/',
+  isThereName,
+  isNameTypeString,
+  isNameValid,
+  isThereAmonut,
+  isThereAmonut,
+  isAmounTypeString,
+  isAmountValid,
+  (req, res) => productController.registerProduct(req, res),
+);
 productRouter.get('/', (_req, res) => productController.getAllProducts(res));
 
 export default productRouter;
