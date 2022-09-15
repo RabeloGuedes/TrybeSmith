17a9990ee8b84104137ab4a24df13d6106f830a0
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-09-14 22:09:09 -0300

    feat: Adiciona middlewares à rota userRouter.

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,10 +1,39 @@
 import express from 'express';
 import UserController from '../controllers/users';
+import {
+  isThereUsername,
+  isUsernameTypeString,
+  isUsernameValid,
+  isThereClasse,
+  isClasseTypeString,
+  isClasseValid,
+  isThereLevel,
+  isLevelTypeNumber,
+  isLevelValid,
+  isTherePassword,
+  isPasswordTypeString,
+  isPasswordValid,
+} from '../middlewares/users';
 
 const userRouter = express.Router();
 
 const userController = new UserController();
 
-userRouter.post('/', (req, res) => userController.registerUser(req, res));
+userRouter.post(
+  '/',
+  isThereUsername,
+  isUsernameTypeString,
+  isUsernameValid,
+  isThereClasse,
+  isClasseTypeString,
+  isClasseValid,
+  isThereLevel,
+  isLevelTypeNumber,
+  isLevelValid,
+  isTherePassword,
+  isPasswordTypeString,
+  isPasswordValid,
+  (req, res) => userController.registerUser(req, res),
+);
 
 export default userRouter;
